f693eef4d2725d85d9003ed8cd8c241b3f92a95c
Jim Maynard Formalejo <jimmaynard.formalejo@gmail.com>
2025-01-10 20:24:43 +0800

    Unedited Grid Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,14 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import './globals.css';
+import { ThemeProvider } from './provider';
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: "Jim's Portfolio",
-  description: "Minimalist Full Stack Developer Portfolio",
+  description: 'A Minimalist Full Stack Dev Portfolio',
 };
 
 export default function RootLayout({
@@ -24,16 +17,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" suppressHydrationWarning>
+      {/*<head>
+        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+      </head>*/}
+      <body className={inter.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
           enableSystem
-          disableTransitionOnChange
-        >
+          disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>
